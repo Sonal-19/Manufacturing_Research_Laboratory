d052e65af15ef0344eb74b73f85356212e455c01
Sonal <sonalchy02@gmail.com>
2024-04-07 17:09:01 +0530

    update

--- Server/index.js
+++ Server/index.js
@@ -6,13 +6,14 @@ const app = express();
 dotenv.config();
 
 const cors = require("cors");
+
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:5173",
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true, 
   optionSuccessStatus: 200,
 };
-
-app.use(cors()); 
+app.use(cors(corsOptions));
 
 app.get("/",(req, res)=>{
   res.send("hello world")
